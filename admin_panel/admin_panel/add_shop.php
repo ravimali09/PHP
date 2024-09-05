3ed6333dc90f525994310e237306aa3bbede015d
Ravi Kumar <ravikumarmali0908@gmail.com>
2024-09-05 16:38:58 +0530

    Changes

---
+++ admin_panel/admin_panel/add_shop.php
@@ -0,0 +1,113 @@
+﻿<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+      <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Free Bootstrap Admin Template : Binary Admin</title>
+	<!-- BOOTSTRAP STYLES-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+     <!-- FONTAWESOME STYLES-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+        <!-- CUSTOM STYLES-->
+    <link href="assets/css/custom.css" rel="stylesheet" />
+     <!-- GOOGLE FONTS-->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+    <div id="wrapper">
+        <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="fa fa-bar"></span>
+                    <span class="fa fa-bar"></span>
+                    <span class="fa fa-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">Binary admin</a> 
+            </div>
+  <div style="color: white;
+padding: 15px 50px 5px 50px;
+float: right;
+font-size: 16px;"> Last access : 30 May 2014 &nbsp; <a href="#" class="btn btn-danger square-btn-adjust">Logout</a> </div>
+        </nav>   
+       
+                 
+         
+     
+<?php
+  include_once('header.php');
+  ?>
+        <div id="page-wrapper">
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="page-head-line">Add Shop</h1>
+                        
+
+                    </div>
+                </div>
+                <!-- /. ROW  -->
+                <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+               <div class="panel panel-info">
+                        <div class="panel-heading">
+                           Add Shop
+                        </div>
+                        <div class="panel-body">
+                            <form role="form" method="post">
+                                        <div class="form-group">
+                                            <label>shop_id</label>
+                                            <input class="form-control" type="number">
+                                            <p class="help-block">Enter Shop ID here.</p>
+                                        </div>
+									<div class="form-group">
+                                            <label>shop_name</label>
+                                            <input class="form-control" type="text">
+                                     <p class="help-block">Enter Shop name here.</p>
+                                        </div>
+                                            <div class="form-group">
+                                            <label>address</label>
+                                            <textarea class="form-control" rows="3"></textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>image</label>
+                                            <input class="form-control" type="file">
+                                            <p class="help-block">Upload File here.</p>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>city_id</label>
+                                            <input class="form-control" type="number">
+                                            <p class="help-block">Enter City ID here.</p>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>google_profile</label>
+                                            <input class="form-control" type="url">
+                                            <p class="help-block">Enter URL here.</p>
+                                        </div>
+                                        <button type="submit" class="btn btn-info">Send Message </button>
+
+                                    </form>
+                            </div>
+                        </div>
+                            </div>
+
+        </div>
+
+            </div>
+            <!-- /. PAGE INNER  -->
+        </div>
+        <!-- /. PAGE WRAPPER  -->
+    </div>
+  
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+    <!-- JQUERY SCRIPTS -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+      <!-- BOOTSTRAP SCRIPTS -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- METISMENU SCRIPTS -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+      <!-- CUSTOM SCRIPTS -->
+    <script src="assets/js/custom.js"></script>
+ 
+</body>
+</html>
